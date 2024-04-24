a6e59b097271142701dc7aae05d95a39b3c63fa1
Šimon Svoboda <apestticka@gmail.com>
2024-04-24 11:03:14 +0200

    WordCounterBase+Tests

---
+++ WordCounter/WordCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace WordCounterNS
+{
+    public class WordCounter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            WordCounter counter = new WordCounter();
+            Console.WriteLine("Input text:");
+            string? text = Console.ReadLine();
+            if (text == null) {
+                Console.WriteLine("Error: No text given");
+                return;
+            }
+            int count = counter.CountWords(text);
+            Console.WriteLine($"The text has {count} words.");
+        }
+
+        public int CountWords(string text)
+        {
+            return 0;
+        }
+    }
+}
