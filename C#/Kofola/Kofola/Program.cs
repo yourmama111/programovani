8202fd9eb92dc121406974e44a388591457e5225
Šimon Svoboda <apestticka@gmail.com>
2024-05-16 11:56:15 +0200

    Better Tests

--- C#/Kofola/Kofola/Program.cs
+++ C#/Kofola/Kofola/Program.cs
@@ -7,26 +7,11 @@
             Console.WriteLine("Hello, World!");
         }
 
-        static string SpocitejVysledek(int cenaVelkeho, int objemVelkeho, int cenaMaleho, int objemMaleho)
+        public static string VyresProblem(int cenaVelkeho, int objemVelkeho, int cenaMaleho, int objemMaleho)
         {
             string vysledek = "NEVIM";
 
             return vysledek;
-        }
-
-        public static void PrectiVstup(TextReader vstup, TextWriter vystup)
-        {
-            int pocetProblemu = int.Parse(vstup.ReadLine()!);
-
-            for (int i = 0; i < pocetProblemu; i++)
-            {
-                int cenaVelkeho = int.Parse(vstup.ReadLine()!);
-                int objemVelkeho = int.Parse(vstup.ReadLine()!);
-                int cenaMaleho = int.Parse(vstup.ReadLine()!);
-                int objemMaleho = int.Parse(vstup.ReadLine()!);
-
-                vystup.WriteLine(SpocitejVysledek(cenaVelkeho, objemVelkeho, cenaMaleho, objemMaleho));
-            }
         }
     }
 }
