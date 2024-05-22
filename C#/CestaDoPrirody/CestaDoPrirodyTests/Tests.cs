82a3d66c51599b97f9b839618a03bbd5fa04d6c2
Šimon Svoboda <apestticka@gmail.com>
2024-05-22 11:43:35 +0200

    Fixed tests...

--- C#/CestaDoPrirody/CestaDoPrirodyTests/Tests.cs
+++ C#/CestaDoPrirody/CestaDoPrirodyTests/Tests.cs
@@ -10,7 +10,7 @@ namespace CestaDoPrirodyTests
         {
             using var input = new StreamReader("TestFiles/tests.txt");
             using var output = new StringWriter();
-            using var expected = new StreamReader("TestFiles/idk.txt");
+            using var expected = new StreamReader("TestFiles/expected.txt");
             Console.SetIn(input);
             Console.SetOut(output);
             Program.Main([]);
